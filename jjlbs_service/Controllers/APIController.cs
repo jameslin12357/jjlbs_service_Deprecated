8d6eaedc4be32a71671e58d555789c16e237e8e3
jameslin12357 <jameslin12357@qq.com>
2019-08-19 09:57:29 -0400

    cm#5

--- jjlbs_service/Controllers/APIController.cs
+++ jjlbs_service/Controllers/APIController.cs
@@ -51,6 +51,45 @@ namespace jjlbs_service.Controllers
                 string json = JsonConvert.SerializeObject(data);
                 return json;
             }
+
+
+        public string EditVillageNoBounds(string id)
+        {
+            if (id == null)
+            {
+                return "404";
+            }
+            string village_name = HttpContext.Request.Form["village_name"];
+            string village_address = HttpContext.Request.Form["village_address"];
+            string village_region = HttpContext.Request.Form["village_region"];
+            string village_type = HttpContext.Request.Form["village_type"];
+            List<string> errors = new List<string>();
+            if (village_name.Length == 0)
+            {
+                errors.Add("小区名称不能为空");
+            }
+            if (village_address.Length == 0)
+            {
+                errors.Add("详细地址不能为空");
+            }
+            if (village_region.Length == 0)
+            {
+                errors.Add("区域不能为空");
+            }
+            if (village_type.Length == 0)
+            {
+                errors.Add("类型不能为空");
+            }
+            if (errors.Count > 0)
+            {
+                return JsonConvert.SerializeObject(errors);
+            } else
+            {
+                Oraclehp ohp = new Oraclehp();
+                DataSet data = ohp.Query($"update lbs_village set village_name ='{village_name}',village_address='{village_address}',village_region='{village_region}',village_type='{village_type}' where village_id = '{id}'");
+                return "[]";
+            }
         }
+    }
     }
 
